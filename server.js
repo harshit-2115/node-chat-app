fe0c3b8121cd5484ef8241268796a6b55398e012
harshit-2115 <harshit.2115@gmail.com>
2019-08-08 19:55:27 +0530

    socket.io added

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const app = express();
+const socketIO = require('socket.io');
+const http = require('http');
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 app.use(express.static('public'));
 
-
-
 app.get('/',(req,res) => {
 	console.log('At localhost');
-	res.send('index.html')
+	res.send('index.html');
 });
 
-app.listen(3000,() => {
+io.on('connection', (socket) => {
+	console.log('New user connected.');
+});
+
+io.on('disconnect', (socket) => {
+	console.log('User connected.');
+});
+
+server.listen(3000,() => {
 	console.log('Listening to port 3000')
 })
