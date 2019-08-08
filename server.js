19a7b140529d8ebbb42d6e7cf008bddfcec89a89
harshit-2115 <harshit.2115@gmail.com>
2019-08-08 21:07:06 +0530

    Greet new user

--- server.js
+++ server.js
@@ -12,18 +12,28 @@ app.use(express.static('public'));
 app.get('/',(req,res) => {
 	console.log('At localhost');
 	res.send('index.html');
-});
+});	
 
 io.on('connection', (socket) => {
 	console.log('New user connected.');
 
 	socket.emit('newMessage', {
-		from : 'GOD',
-		text : 'You created me'
-	});
+		from: 'Admin',
+		text: 'Welcome to the chat app'
+	})
+
+	socket.broadcast.emit('newMessage', {
+		from : 'Admin',
+		text : 'New user connected'
+	})
 
 	socket.on('createMessage',(message) => {
 	console.log('New Message', message);
+
+	io.emit('newMessage',{
+		from : message.from,
+		text : message.text
+	})
 });
 
 });
