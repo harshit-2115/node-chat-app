fbd9d0ca2474236029bb0972380c4fbcd8006588
harshit-2115 <harshit.2115@gmail.com>
2019-08-09 19:04:20 +0530

    AAA

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,7 +3,7 @@ var socket = io();
 
 socket.on('connect', function(e) {
     var params = jQuery.deparam(window.location.search)
-    
+
     socket.emit('join', params);
     console.log('connect');
 });
@@ -13,13 +13,13 @@ socket.on('disconnect', function() {
     console.log('disconnect');
 });
 
-socket.on('!join', function(){
+socket.on('!join', function() {
     alert('Invalid Entries')
     window.location.href = '/';
 });
 
 socket.on('newMessage', function(message) {
-    
+
     var formattedTime = moment(message.createdAt).format('h:mm:ss a')
 
     var li = jQuery('<li></li>');
@@ -27,18 +27,18 @@ socket.on('newMessage', function(message) {
     jQuery('#messages').append(li);
 });
 
-socket.on('clear',function(){
+socket.on('clear', function() {
     jQuery('[name=message]').val('')
 });
 
 
 
-jQuery('#message-form').on('submit', function(e){
+jQuery('#message-form').on('submit', function(e) {
     e.preventDefault()
-    
-    socket.emit('createMessage',{
+
+    socket.emit('createMessage', {
         from: 'User',
         text: jQuery('[name=message]').val()
     });
 
-});	
+});
--- server.js
+++ server.js
@@ -5,51 +5,47 @@ const http = require('http');
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-const {generateMessage} = require('./utils/message')
-const {isRealString} = require('./utils/validation')
+const {
+    generateMessage
+} = require('./utils/message')
+const {
+    isRealString
+} = require('./utils/validation')
 
 app.use(express.static('public'));
 
-app.get('/',(req,res) => {
-	console.log('At localhost');
-	res.send('join.html');
-});	
+app.get('/', (req, res) => {
+    console.log('At localhost');
+    res.send('join.html');
+});
 
 io.on('connection', (socket) => {
-	console.log('New user connected.');
+    console.log('New user connected.');
 
 
-	socket.on('join',(params,callback) => {
-		if(!isRealString(params.name) || !isRealString(params.room))
-		{
-			console.log('False')
-			socket.emit('!join');
-			
-		}
+socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+        console.log('False')
+        socket.emit('!join');
+	}
 
-		socket.join(params.room);
-		// socket.leave('')
-		socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
-		socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin',`${params.name} has joined`));
-		
+    socket.join(params.room);
+    // socket.leave('')
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+
+});
+
+socket.on('createMessage', (message, callback) => {
+    io.emit('newMessage', generateMessage(message.from, message.text))
+    	socket.emit('clear');
 	});
-
-
-	
-
-	socket.on('createMessage',(message, callback) => {
-		io.emit('newMessage',generateMessage(message.from,message.text))
-		socket.emit('clear');
-	});
-
 });
 
 io.on('disconnect', (socket) => {
-	console.log('User connected.');
+    console.log('User connected.');
 });
 
-
-
-server.listen(3000,() => {
-	console.log('Listening to port 3000')
+server.listen(3000, () => {
+    console.log('Listening to port 3000')
 })
